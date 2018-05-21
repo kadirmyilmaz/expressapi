85e737bddc090bc50a22952bbc9caa80fab63976
bahagiga <bahagiga@users.noreply.github.com>
2018-05-21 19:19:48 +0200

    :zap: Add debug script and update www startup file

--- app.js
+++ app.js
@@ -1,42 +1,42 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var mustacheExpress = require('mustache-express');
-var nodemon = require('nodemon');
+import express from "express";
+import { join } from "path";
+import favicon from "serve-favicon";
+import logger from "morgan";
+import cookieParser from "cookie-parser";
+import { json, urlencoded } from "body-parser";
+import mustacheExpress from "mustache-express";
+import nodemon from "nodemon";
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+import index from './routes/index';
+import users from "./routes/users";
 
-var app = express();
+const app = express();
 
 app.engine('mustache', mustacheExpress());
 
 // view engine 
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', join(__dirname, 'views'));
 app.set('view engine', 'mustache');
 
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(json());
+app.use(urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(join(__dirname, '../public')));
 
-app.use('/', index.default);
+app.use('/', index);
 app.use('/users', users);
- 
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -46,4 +46,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+export default app;
