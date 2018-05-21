2c7348b1b862b63fd5131db1e95511b0f2d8b234
bahagiga <bahagiga@users.noreply.github.com>
2018-05-21 18:23:14 +0200

    :construction: WIP es6 module syntax, add favicon and load public asset folder

--- app.js
+++ app.js
@@ -18,15 +18,14 @@ app.engine('mustache', mustacheExpress());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'mustache');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
-app.use('/', index);
+app.use('/', index.default);
 app.use('/users', users);
  
 // catch 404 and forward to error handler
